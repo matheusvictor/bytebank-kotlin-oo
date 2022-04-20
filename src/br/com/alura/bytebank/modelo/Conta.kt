6963714fa4fa9035f6de054ce0d3eee29c2e97f8
Matheus Victor <matheusvictor.salles@gmail.com>
2022-04-20 09:36:23 -0300

    Implementa contador de Contas criadas
    O uso do companion object permite a criação de uma instância única de um objeto como membro de uma classe.

--- src/br/com/alura/bytebank/modelo/Conta.kt
+++ src/br/com/alura/bytebank/modelo/Conta.kt
@@ -7,6 +7,17 @@ abstract class Conta(
     var saldo = 0.0
         protected set
 
+    companion object Contador {
+        var totalContasCriadas: Int = 0
+            private set
+    }
+
+    // esse bloco de código será executado sempre que for feita uma instância de Conta
+    init {
+        println("Criando uma conta...")
+        totalContasCriadas++
+    }
+
     fun deposita(valor: Double) {
         if (valor > 0) this.saldo += valor
     }
