8f80ab731564076ecc8ab86cab6f725b66025be7
Matheus Victor <matheusvictor.salles@gmail.com>
2022-04-20 14:56:31 -0300

    Add comentário sobre uso do Smart Cast
    Para manipulação de parâmetro do tipo Any

--- src/br/com/alura/bytebank/modelo/CalculadoraBonificacao.kt
+++ src/br/com/alura/bytebank/modelo/CalculadoraBonificacao.kt
@@ -9,4 +9,14 @@ class CalculadoraBonificacao {
         this.total += funcionario.bonificacao
     }
 
+    // função registra recebendo um parâmetro do tipo Any
+    /*
+        fun registra(funcionario: Any) {
+            // o smart cast implementado no if é o que garante que esse Any seja do tipo Funcionario
+            if (funcionario is Funcionario)
+                this.total += funcionario.bonificacao
+        }
+     */
+
+
 }
