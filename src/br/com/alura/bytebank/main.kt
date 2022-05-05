3be9539db09b3f2afe8fb676cccd38da959cd965
Matheus Victor <matheusvictor.salles@gmail.com>
2022-05-05 10:59:09 -0300

    Implementa exemplos de variável e Classe do tipo função

--- src/br/com/alura/bytebank/main.kt
+++ src/br/com/alura/bytebank/main.kt
@@ -1,5 +1,31 @@
 fun main() {
-    println("inicio da função main")
-    testaComportamentosConta()
-    println("fim da função main")
+
+    /*
+        Variável do tipo função:
+        a inicialização de uma variável do tipo função precisa ser "assinada" a partir de uma
+        função já existente e que seja compatível com a assinatura da função existente.
+
+        :: indica a referência da função a ser usada. Isto é, isso não indica a execução função, mas sim a assinatura
+    */
+
+    val minhaFuncao: () -> Unit = ::teste
+    println(minhaFuncao) // imprime o tipo da variável minhaFuncao
+    println(minhaFuncao()) // "executa" a variável
+
+    val minhaFuncaoClasse = MinhaClasseComTipoFuncao() // como Classe já trabalham com referência, não usa-se o ::
+    println("Referência da classe do tipo função: $minhaFuncaoClasse")
+    println("Execuçaõ da função: ${minhaFuncaoClasse()}")
+
+}
+
+fun teste() {
+    println("Executa função de teste")
+}
+
+class MinhaClasseComTipoFuncao : () -> Unit {
+
+    override fun invoke() {
+        println("Executa invoke da MinhaClasseComTipoFuncao")
+    }
+
 }
