4453702f1ec445202a5028da2758b3ce379422d0
Matheus Victor <matheusvictor.salles@gmail.com>
2022-05-06 11:41:17 -0300

    Add exemplos de uso de higher order functions

--- src/br/com/alura/bytebank/main.kt
+++ src/br/com/alura/bytebank/main.kt
@@ -1,67 +1,18 @@
+import br.com.alura.bytebank.modelo.Endereco
+
 fun main() {
 
-//    testaTipoFuncaoReferencia()
+    // uso de higher order functions
+    Endereco(logradouro = "Rua dos Bobos", numero = 0)
+        .let { endereco ->
+            "${endereco.logradouro}, ${endereco.numero}".toUpperCase()
+        }.let(::println) // invocação da função println por referência, recebendo o retorno do primeiro let como parâmetro
 
-    val minhaFuncaoLambda: (Int, Int) -> Int =
-        { a, b -> a + b } // em funções lambdas a última expressão equivale ao retorno da função
-    println(minhaFuncaoLambda(20, 20))
-
-    /*
-        Obs. Em funções lambdas, o _ recebido como parâmetro significa que aquele parâmetro em específico
-        não será usado na função
-    */
-
-    val minhaFuncaoAnonima: (Int, Int) -> Int = fun(a, b): Int { // função anônima
-        println("Executa como função anônima")
-        return a + b
-    }
-    println(minhaFuncaoAnonima(1, 1))
-
-    val calculaBonificacaoFuncaoAnonima: (salario: Double) -> Double = fun(salario): Double {
-        if (salario > 1000.0) {
-            return salario + 50.0
-        }
-        return salario + 100.0
-    }
-
-    println(calculaBonificacaoFuncaoAnonima(1000.0))
-    println(calculaBonificacaoFuncaoAnonima(1100.0))
-
-    val calculaBonificacaoLambda: (salario: Double) -> Double = funcaoLambda@{ salario ->
-        if (salario > 1000.0) {
-            return@funcaoLambda salario + 50.0
-        }
-        return@funcaoLambda salario + 100.0
-    }
-
-    println(calculaBonificacaoLambda(1000.0))
-    println(calculaBonificacaoLambda(1100.0))
-
-}
-
-fun testaTipoFuncaoClasse() {
-    val minhaFuncaoClasse: (Int, Int) -> Int = Soma() // como Classe já trabalham com referência, não usa-se o ::
-    println("Execução da função (chamada do invoke): ${minhaFuncaoClasse(2, 2)}")
-}
-
-fun testaTipoFuncaoReferencia() {
-    /*
-        Variável do tipo função:
-        a inicialização de uma variável do tipo função precisa ser "assinada" a partir de uma
-        função já existente e que seja compatível com a assinatura da função existente.
-
-        :: indica a referência da função a ser usada. Isto é, isso não indica a execução função, mas sim a assinatura
-    */
-
-    val minhaFuncao: (Int, Int) -> Int = ::somar
-    println(minhaFuncao) // imprime o tipo da variável minhaFuncao
-    println(minhaFuncao(5, 10)) // "executa" a variável
-}
-
-fun somar(a: Int, b: Int): Int {
-    return a + b
-}
-
-class Soma : (Int, Int) -> Int {
-    override fun invoke(a: Int, b: Int): Int = a + b
+    listOf(
+        Endereco(complemento = "Casa"),
+        Endereco(),
+        Endereco(complemento = "Apartamento")
+    )
+        .filter { endereco -> endereco.complemento.isNotEmpty() } // lista endereços que possuem complemento
+        .let(::println) // invocação da função println por referência, recebendo o retorno do filter (neste caso, lista endereços que possuem complemento)
 }
