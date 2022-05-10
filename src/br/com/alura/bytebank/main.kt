f5c0794e9c1c2d7456d3dfdcd8983f7d53c47c58
Matheus Victor <matheusvictor.salles@gmail.com>
2022-05-10 08:21:55 -0300

    Add exemplos de uso de higher order functions

--- src/br/com/alura/bytebank/main.kt
+++ src/br/com/alura/bytebank/main.kt
@@ -1,4 +1,6 @@
+import br.com.alura.bytebank.modelo.Autenticavel
 import br.com.alura.bytebank.modelo.Endereco
+import br.com.alura.bytebank.modelo.SistemaInterno
 
 fun main() {
 
@@ -6,7 +8,8 @@ fun main() {
     Endereco(logradouro = "Rua dos Bobos", numero = 0)
         .let { endereco ->
             "${endereco.logradouro}, ${endereco.numero}".toUpperCase()
-        }.let(::println) // invocação da função println por referência, recebendo o retorno do primeiro let como parâmetro
+        }
+        .let(::println) // invocação da função println por referência, recebendo o retorno do primeiro let como parâmetro
 
     listOf(
         Endereco(complemento = "Casa"),
@@ -15,4 +18,18 @@ fun main() {
     )
         .filter { endereco -> endereco.complemento.isNotEmpty() } // lista endereços que possuem complemento
         .let(::println) // invocação da função println por referência, recebendo o retorno do filter (neste caso, lista endereços que possuem complemento)
+
+    soma(1, 5, resultado = { println(it) })
+
+    val exemploAutenticavel = object : Autenticavel {
+        val senha: Int = 1234
+        override fun autentica(senha: Int): Boolean = this.senha == senha
+    }
+
+    SistemaInterno().entra(exemploAutenticavel, 1234)
+
+}
+
+fun soma(a: Int, b: Int, resultado: (Int) -> Unit) {
+    resultado(a + b)
 }
--- src/br/com/alura/bytebank/modelo/SistemaInterno.kt
+++ src/br/com/alura/bytebank/modelo/SistemaInterno.kt
@@ -2,10 +2,11 @@ package br.com.alura.bytebank.modelo
 
 class SistemaInterno {
 
-    fun entra(admin: Autenticavel, senha: Int) {
-        if (admin.autentica(senha))
+    fun entra(admin: Autenticavel, senha: Int, autenticado: () -> Unit = {}) {
+        if (admin.autentica(senha)) {
             println("Boas-vindas ao ByteBank!")
-        else
+            autenticado()
+        } else
             println("Falha ao tentar autenticar!")
     }
 
